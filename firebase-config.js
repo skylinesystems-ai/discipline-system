450a7069051435964de9e5f479bf8dec5d181579
RunVision <contatomariofarias@gmail.com>
2026-05-30 09:18:48 -0400

    Add Firebase configuration settings

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+export const firebaseSettings = {
+    useFirebase: true,
+    config: {
+        apiKey: "AIzaSyCnBF6z_OeASjKrg300WxA5iATmwKFEBsQ",
+        authDomain: "discipline-system-7d201.firebaseapp.com",
+        projectId: "discipline-system-7d201",
+        storageBucket: "discipline-system-7d201.firebasestorage.app",
+        messagingSenderId: "116356548205",
+        appId: "1:116356548205:web:0610eb6c14f5aeb08d2164",
+    },
+};
